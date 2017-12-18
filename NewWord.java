1f955265d3eb26482aa8af0d056207aa9459d365
NuxTuxSux <turtulici.nunzio@spes.uniud.it>
2017-12-18 20:49:36 +0100

    boh

--- NewWord.java
+++ NewWord.java
@@ -4,27 +4,27 @@ public class NewWord {
 
 	class Pair {
 		public char c;
-		public short n;
-		public Pair(char x, short y) {
+		public int n;
+		public Pair(char x, int y) {
 			c = x;
 			n = y;
 		}
 	};
 
-	List<Occurrence> occurrences;
+	List<Pair> occurrences;
 
 	public void add(char c) {
-		for (char occ: occurrences)
+		for (Pair occ: occurrences)
 			if (occ.c == c) {
 				occ.n++;
 				return;
 			}
 
-		pair.add(new Pair(c,1));
+		occurrences.add(new Pair(c,1));
 	}
 
-	public short get(char c) {
-		for (char occ: occurrences)
+	public int get(char c) {
+		for (Pair occ: occurrences)
 			if (occ.c == c)
 				return occ.n;
 		return 0;
@@ -33,7 +33,7 @@ public class NewWord {
 	public NewWord(String raw) {
 		occurrences = new ArrayList<Pair>();
 
-		for (char c: raw)
+		for (char c: raw.toCharArray())
 			add(c);
 	}
 
