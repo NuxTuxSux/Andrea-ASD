f4287efcef857a583d90a1f56aec6019e8e032ed
NuxTuxSux <turtulici.nunzio@spes.uniud.it>
2017-12-18 21:26:24 +0100

    ArrayAssociativi-fatto

--- testNewWord.java
+++ testNewWord.java
@@ -8,6 +8,30 @@ class testNewWord {
 		String prova = scanner.next();
 		NewWord diz = new NewWord(prova);
 		System.out.println(diz);
+
+		System.out.println("\n----Prova confronto----");
+
+		String cmd = "";
+		NewWord prima, seconda;
+		while(!cmd.equals("x")) {
+			System.out.print("Prima:");
+			prima = new NewWord(scanner.next());
+			System.out.print("Seconda:");
+			seconda = new NewWord(scanner.next());
+			if (prima.leq(seconda)) {
+				if (seconda.leq(prima)) {
+					System.out.println("Le due parole sono anagrammi");
+				} else {
+					System.out.println("La prima parola è più piccola");
+				}
+			} else if (seconda.leq(prima)) {
+				System.out.println("La seconda parola è più piccola");
+			} else {
+				System.out.println("Le parole non sono confrontabili");
+			}
+
+			cmd = scanner.next();
+		}
 	}
 
 }
