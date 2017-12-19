fdb36f62c880edd57e8b5fe099eca5986c3894db
NuxTuxSux <turtulici.nunzio@spes.uniud.it>
2017-12-19 18:17:13 +0100

    Definire altezza

--- DiGraph.java
+++ DiGraph.java
@@ -5,15 +5,27 @@ class DiGraph {
 	public int N;
 	private List<List<Integer>> sons;
 	private List<Integer> heights;
+	//public int
 
 	public DiGraph(int N) {
 		this.N = N;
-		this.sons = new ArrayList<List<Integer>>();
-		for (List<Integer> nodeSons: this.sons)
+		this.sons = new ArrayList<List<Integer>>(N);
+		for (List<Integer> nodeSons: this.sons) {
 			nodeSons = new ArrayList<Integer>();
-			
+			heights.add(-1);
+		}
 	}
 
+	public void connect(int parent, int son) {
+		if (parent < N)
+			sons.get(parent).add(son);
+	}
 
+	public int height(int node) {
+		int h = heights.get(node);
+		if (h > -1)
+			return h;
+		
+	}
 
 }
